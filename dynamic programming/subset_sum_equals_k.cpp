9bfdaf3609822772fa7005cffad7f21aac34bbf6
abhi <abhi.badam186@gmail.com>
2023-07-16 00:05:44 +0530

    added 1 more question to dynamic programming

---
+++ dynamic programming/subset_sum_equals_k.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h> 
+//  >>>>>>>> MEMOIZATION 
+// bool helper(int n,int i,int k,vector<int>& arr,vector<int>& mem)
+// {
+//     if(k==0)
+//     {
+//         return true;
+//     }
+//     if(k<0||i>=n)
+//     {
+//         return false;
+//     }
+//     if(mem[k]!=-1)
+//     {
+//         if(mem[k]==1)
+//         {
+//             return true;
+//         }else
+//         {
+//             return false;
+//         }
+//     }
+//     int a=int(helper(n,i+1,k-arr[i],arr,mem));
+//     int b=int(helper(n,i+1,k,arr,mem));
+//     if(a==1||b==1)
+//     {
+//         mem[k]=1;
+//     }
+//     else{
+//         mem[k]=0;
+//     }
+
+//     return bool(mem[k]);
+
+// }
+// bool subsetSumToK(int n, int k, vector<int> &arr) {
+//      vector<int> mem(k+1,-1);
+//     return helper(n,0,k,arr,mem);
+// }
+// >>>>>>>>TABULATION ,SPACE OPTIMIZATION
+bool subsetSumToK(int n,int k,vector<int> &arr)
+{
+    if(k==0)
+    {
+        return true;
+    }
+    if(k<0||n<=0)
+    {
+        return false;
+    }
+   vector<int> ind;
+   ind.push_back(0);
+    for(int i=0;i<n;i++)
+    {
+        int m=ind.size();
+       for(int j=0;j<m;j++) {
+        if(arr[i]+ind[j]<=k)
+        {   
+            ind.push_back(arr[i]+ind[j]);
+            if(arr[i]+ind[j]==k)
+            {
+                return true;
+            }
+            
+        }
+       }
+    }
+    return false;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    int k;
+    cin>>k;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<subsetSumToK(n,k,arr);
+    return 0;
+}
